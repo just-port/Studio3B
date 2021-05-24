849b83eb60715750e89a684880a9090afec6978d
Justin Portell <justinportell84@gmail.com>
2021-05-24 13:06:06 -0500

    Initial commit

---
+++ src/com/company/quiz/question/trueorfalse/TrueOrFalse.java
@@ -0,0 +1,33 @@
+package com.company.quiz.question.trueorfalse;
+
+public class TrueOrFalse {
+    private final int questionId;
+    private String prompt;
+    private boolean answer;
+
+    public TrueOrFalse(int questionId, String prompt, boolean answer) {
+        this.questionId = questionId;
+        this.prompt = prompt;
+        this.answer = answer;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public void setPrompt(String aPrompt) {
+        prompt = aPrompt;
+    }
+
+    public void setAnswer(boolean anAnswer) {
+        answer = anAnswer;
+    }
+}
